90af1651884a3d9c0008bc9447928d0ba5beab7b
Hasnain <hasnainr0111@gmail.com>
2026-01-06 22:57:22 +0500

    fix

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ connectDB();
 const app = express();
 
 // Body parser
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Enable CORS
 const allowedOrigins = [
